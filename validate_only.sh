5bdc22dc3f7c346a85cbe84eb6aac7e65a698258
Tatyana <tatyana222v@gmail.com>
2026-05-02 19:41:42 +0500

    Fix validation script

--- validate_only.sh
+++ validate_only.sh
@@ -1,7 +1,7 @@
 python validate_only.py \
   --val_set "/home/jovyan/AndreiZ/MARL/TatyanaG/swarm_data_npy" \
   --val_set_list val.json \
-  --resume_model_path ./training_log/diffusion-planner-training/2026-05-01-22:04:37/latest.pth \
+  --resume_model_path ./training_log/diffusion-planner-training/2026-05-01-22:04:37 \
   #--batch_size 64 \
   --num_workers 8 \
   --quick_val_experiments 5
